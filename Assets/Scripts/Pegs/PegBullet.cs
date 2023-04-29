41c75e38c6cffdfcd18ecbb69ef8c1ba0c8a9760
Allinius <michal.durovec@gmail.com>
2023-04-29 20:26:27 +0200

    add peg trajectory calculation

--- Assets/Scripts/Pegs/PegBullet.cs
+++ Assets/Scripts/Pegs/PegBullet.cs
@@ -10,25 +10,65 @@ public class PegBullet : MonoBehaviour
 
     private Rigidbody2D rb;
     private bool isMoving = false;
+    [SerializeField] private GameObject trajectoryDotsParent;
+    private Transform[] trajectoryDots;
+
+    private Vector2 direction;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+
+        if (trajectoryDotsParent != null)
+        {
+            trajectoryDots = trajectoryDotsParent.GetComponentsInChildren<Transform>();
+        }
     }
 
     private void FixedUpdate()
     {
         if (Input.GetMouseButton(0) && !isMoving)
         {
-            Vector3 mousePos = Input.mousePosition;
-            mousePos.z = Camera.main.nearClipPlane;
-            Vector3 objectPos = Camera.main.WorldToScreenPoint(transform.position);
-            Vector3 direction = mousePos - objectPos;
-
             rb.gravityScale = gravityScale.value;
             rb.AddForce(direction.normalized * moveSpeed.value, ForceMode2D.Impulse);
             isMoving = true;
             bulletCount.SetValue(Mathf.Max(0, bulletCount.value - 1));
+
         }
+
+    }
+
+    private void Update()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        direction = new Vector2(mousePosition.x - transform.position.x, mousePosition.y - transform.position.y).normalized;
+        DrawTrajectory();
+    }
+
+    private void DrawTrajectory()
+    {
+        if (isMoving || trajectoryDots.Length == 0)
+        {
+            trajectoryDotsParent.SetActive(false);
+            return;
+        }
+        trajectoryDotsParent.SetActive(true);
+
+        float t = 0f;
+        foreach (Transform dot in trajectoryDots)
+        {
+            Vector2 pathPosition = CalculateParabolicPosition(transform.position, direction, moveSpeed.value, t);
+            dot.position = new Vector3(pathPosition.x, pathPosition.y, -1f);
+            t += 0.05f;
+        }
+
+    }
+
+    private Vector2 CalculateParabolicPosition(Vector2 start, Vector2 direction, float force, float time)
+    {
+        Vector2 result = start;
+        result += direction * force * time;
+        result.y += 0.5f * Physics2D.gravity.y * time * time;
+        return result;
     }
 }
