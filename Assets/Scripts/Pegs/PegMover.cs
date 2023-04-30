dee92577605bf8aea3afdc516ea8659ad8e3a620
Saviqq <patriksavka@gmail.com>
2023-04-30 15:06:55 +0200

    peg mover and rotator

---
+++ Assets/Scripts/Pegs/PegMover.cs
@@ -0,0 +1,53 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PegMover : MonoBehaviour
+{
+    [SerializeField] private Transform pegToMove;
+    [SerializeField] private List<Transform> travelPoints;
+    [Tooltip("Speed to move from one travel point to another")]
+    [SerializeField] private float time = 1f;
+    [Tooltip("Whether to continously loop peg travel")]
+    [SerializeField] private bool loop = true;
+
+    private Vector3[] positions;
+
+    private void Start()
+    {
+        positions = new Vector3[travelPoints.Count];
+        for (int i = 0; i < positions.Length; i++)
+        {
+            positions[i] = travelPoints[i].position;
+        };
+
+
+        StartCoroutine(MovePeg());
+    }
+
+    private IEnumerator MovePeg()
+    {
+        do
+        {
+            yield return TraversePoints();
+        } while (loop);
+    }
+
+    private IEnumerator TraversePoints()
+    {
+        for (int i = 0; i < positions.Length; i++)
+        {
+            yield return MovePegToNextPoint(i);
+        }
+    }
+
+    private IEnumerator MovePegToNextPoint(int index)
+    {
+        while (pegToMove.position != positions[index])
+        {
+            pegToMove.localPosition = Vector3.MoveTowards(pegToMove.localPosition, positions[index], Time.deltaTime / time);
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Scripts/Pegs/PegRotator.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class PegRotator : MonoBehaviour
+{
+    private enum LOOP_DIRECTION
+    {
+        ROTATE_BACK,
+        ROTATE_FORWARD
+    }
+
+    [SerializeField] private float angle = 360f;
+    [Tooltip("Time to rotate, in seconds")]
+    [SerializeField] private float time = 2f;
+    [Tooltip("Whether to continously rotate pegs")]
+    [SerializeField] private bool loop = true;
+    [Tooltip("If looping, dictate rotate direction of the next loop, BACK = return to angle of previous loop, FORWARD = continue applying angle")]
+    [SerializeField] private LOOP_DIRECTION loopDirection = LOOP_DIRECTION.ROTATE_FORWARD;
+
+    private void Start()
+    {
+        RotatePegs();
+    }
+
+    private LTDescr RotatePegs()
+    {
+        if (loop)
+        {
+            if (loopDirection == LOOP_DIRECTION.ROTATE_BACK)
+            {
+                return RotateAround().setLoopPingPong();
+            }
+            return RotateAround().setLoopCount(-1);
+        }
+        return RotateAround();
+    }
+
+    private LTDescr RotateAround()
+    {
+        return LeanTween.rotateAround(gameObject, new Vector3(0f, 0f, 1f), angle, time);
+    }
+}
